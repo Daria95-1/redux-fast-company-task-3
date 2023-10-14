06610d74bcea528f152d59f5e809b4aec856774e
Daria Basynina <dariabasynina@gmail.com>
2023-10-14 18:17:29 +0300

    Удалены файлы, которые были помечены как deleted

--- Desktop/task/fc/src/app/components/ui/qualities/quality.jsx
+++ Desktop/task/fc/src/app/components/ui/qualities/quality.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-const Quality = ({ color, name, _id }) => {
+import { useQualities } from '../../../hooks/useQualities'
+
+const Quality = ({ id }) => {
+    const { getQuality } = useQualities()
+    const quality = getQuality(id)
+
+    if (!quality) {
+        console.log(quality)
+        return 'loading...'
+    }
+
+    const { color, name } = quality
     return <span className={'badge m-1 bg-' + color}>{name}</span>
 }
 Quality.propTypes = {
-    color: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    _id: PropTypes.string.isRequired
+    id: PropTypes.string.isRequired
 }
 
 export default Quality
