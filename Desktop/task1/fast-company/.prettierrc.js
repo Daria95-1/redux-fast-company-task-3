5d5b3fbe47b7462d514158fd99f3e351e383c1e6
Daria Basynina <dariabasynina@gmail.com>
2023-10-22 13:36:55 +0300

    Удалены файлы навсегда

--- Desktop/task1/fast-company/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    trailingComma: 'none',
-    tabWidth: 4,
-    useTabs: false,
-    semi: false,
-    singleQuote: true,
-    bracketSpacing: true,
-    arrowParens: 'always'
-}
