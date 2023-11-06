c8634e3f4fd46d73e0c1f1b27a699b39433e16ef
Daria Basynina <dariabasynina@gmail.com>
2023-11-06 20:37:12 +0300

    task 2

---
+++ Desktop/redux-fast-company-task-2/fast-company/src/app/store/createStore.js
@@ -0,0 +1,15 @@
+import professionsReducer from './professions'
+import qualitiesReducer from './qualities'
+import usersReducer from './users'
+
+const { combineReducers, configureStore } = require('@reduxjs/toolkit')
+
+const rootReducer = combineReducers({
+    qualities: qualitiesReducer,
+    professions: professionsReducer,
+    users: usersReducer
+})
+
+export function createStore() {
+    return configureStore({ reducer: rootReducer })
+}
